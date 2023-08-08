b9fb77bf0739a9572d3581f41a22b405310a816a
Reed Hillmar <reedhillmar@gmail.com>
2023-08-08 10:27:08 -0600

    test: add test code for Market.total_inventory

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -36,24 +36,30 @@ describe Market do
     end 
   end
 
-  describe "#vendor_names" do
-    it "can return the names of vendors at the market" do
+  context "#vendor info" do
+    before(:each) do
       @market.add_vendor(@vendor1)
       @market.add_vendor(@vendor2)
       @market.add_vendor(@vendor3)
-
-      expect(@market.vendor_names). to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
-  end
 
-  describe "#vendors_that_sell" do
-    it "can return the vendors that sell a given item" do
-      @market.add_vendor(@vendor1)
-      @market.add_vendor(@vendor2)
-      @market.add_vendor(@vendor3)
+    describe "#vendor_names" do
+      it "can return the names of vendors at the market" do
+        expect(@market.vendor_names). to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+      end
+    end
 
-      expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
-      expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+    describe "#vendors_that_sell" do
+      it "can return the vendors that sell a given item" do
+        expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
+        expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+      end
+    end
+
+    describe "#total_inventory" do
+      it "can return the quantity of all items sold at the market" do
+        expect(@market.total_inventory).to eq({@item1 => {"quantity" => 100, "vendors" => [@vendor1, @vendor3]}, @item2 => {"quantity" => 7, "vendors" => [@vendor1]}, @item3 => {"quantity" => 25, "vendors" => [@vendor2]}, @item4 => {"quantity" => 50, "vendors" => [@vendor2]}})
+      end
     end
   end
 end

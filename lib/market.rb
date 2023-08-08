9dfaaa665a0cb1d0e536d9c593427d3ca21f5531
Reed Hillmar <reedhillmar@gmail.com>
2023-08-08 10:11:00 -0600

    feat: add funtionality to return all vendors that sell a given item

--- lib/market.rb
+++ lib/market.rb
@@ -15,4 +15,8 @@ class Market
   def vendor_names
     @vendors.map {|vendor| vendor.name}
   end
+
+  def vendors_that_sell(item)
+    @vendors.find_all {|vendor| vendor.inventory.has_key?(item)}
+  end
 end

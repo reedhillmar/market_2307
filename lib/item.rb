e34882db3f8e37a51a0a186eb187a889243724ad
Reed Hillmar <reedhillmar@gmail.com>
2023-08-08 09:13:49 -0600

    feat: add Item class and initialize

--- lib/item.rb
+++ lib/item.rb
@@ -1,1 +1,10 @@
 # ./lib/item.rb
+
+class Item
+  attr_reader :name, :price
+
+  def initialize(item_details)
+    @name = item_details[:name]
+    @price = item_details[:price].sub("$", "").to_f
+  end 
+end

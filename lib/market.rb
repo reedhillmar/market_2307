975cb15868fb74d94d2485d5d463fbb54195c102
Reed Hillmar <reedhillmar@gmail.com>
2023-08-08 11:37:20 -0600

    feat: add date funtionality to Market

--- lib/market.rb
+++ lib/market.rb
@@ -1,11 +1,13 @@
 # ./lib/market.rb
+require "date"
 
 class Market
-  attr_reader :name, :vendors
+  attr_reader :name, :vendors, :date
 
   def initialize(name)
     @name = name 
     @vendors = []
+    @date = Date.today.strftime("%d/%m/%Y")
   end
 
   def add_vendor(vendor)

5b2e425c7a52fcff6a483afd9079c0aeef632c6f
Reed Hillmar <reedhillmar@gmail.com>
2023-08-08 09:25:19 -0600

    test: add test code for Vendor.stock

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -21,4 +21,21 @@ describe Vendor do
       expect(@vendor.check_stock(@item1)).to be 0
     end
   end
+
+  describe "#stock" do
+    it "can add to the stock of an item" do
+      @vendor.stock(@item1, 30)
+
+      expect(@vendor.inventory).to eq({@item1 => 30})
+      expect(@vendor.check_stock(@item1)).to be 30
+
+      @vendor.stock(@item1, 25)
+
+      expect(@vendor.check_stock(@item1)).to be 55
+
+      @vendor.stock(@item2, 12)
+
+      expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
+    end
+  end
 end
